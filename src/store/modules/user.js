96f7c60a895d35516b48654f04c7eafe9a026e2a
former <1633852615@qq.com>
2022-12-30 19:55:32 +0800

    登录页面逻辑完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,36 @@
-export default {
-  namespeced:true,
-  state: {},
-  mutations: {},
-  actions: {}
+import { getToken, setToken, removeToken } from "@/utils/auth"
+import { login } from "@/api/user"
+// 状态
+const state = {
+  token: getToken // 设置token为共享状态
+
 }
+// 修改状态
+const mutations = {
+  setToken(state, token) {
+    state.token = token  // 将数据设置给vuex
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null // 将vuex的数据置空
+    removeToken()  // 同步到缓存
+  }
+}
+
+const actions = {
+ async login(context, data) {
+    // 调用api接口
+   const result = await login(data) // 拿到token
+ 
+     context.commit("setToken",result)  // 设置token
+   
+  }
+}
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,23 @@
 import axios from "axios";
-const service = axios.create()
-
+import { Message } from "element-ui";
+const service = axios.create({
+    baseURL:process.env.VUE_APP_BASE_API,
+    timeout:5000 // 设置超时时间
+})
+// 响应拦截器
 service.interceptors.request.use()
-service.interceptors.response.use()
+service.interceptors.response.use(response =>{
+    // axios默认加了一层data
+  const {success,message,data} = response.data
+  if(success){
+    return data
+  }else{
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+},error =>{
+    Message.error(error.message)   // 提示错误信息
+    return Promise.reject(error)   // 返回执行错误，让当前的执行链跳出成功，直接进入catch
+    
+})
 export default service
