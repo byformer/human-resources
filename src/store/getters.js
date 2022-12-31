851eaaeb16cf2e4984a8a68d649df5c0b01ee1f9
former <1633852615@qq.com>
2022-12-31 11:50:52 +0800

    主页导航样式

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,8 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token:state => state.user.token   // 建立token的快捷访问
+  token: state => state.user.token,   // 建立token的快捷访问
+  avatar: state => state.app.avatar,
+  name:state => state.app.name,
 }
 export default getters
