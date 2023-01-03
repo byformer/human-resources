deb31dba6df5382b74d0666dbc76fba0ddcd755c
former <1633852615@qq.com>
2023-01-03 11:12:12 +0800

    合并数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { Message } from "element-ui";
 import store from "@/store";
 const service = axios.create({
-    baseURL:process.env.VUE_APP_BASE_API,
-    timeout:5000 // 设置超时时间
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000 // 设置超时时间
 })
 // 请求拦截器
 
@@ -16,22 +16,23 @@ service.interceptors.request.use(config => {
   return config // 必须返回配置
 }, error => {
   return Promise.reject(error)
-}) 
+})
 // 响应拦截器
 service.interceptors.response.use(
-  response =>{
+  response => {
     // axios默认加了一层data
-  const {success,message,data} = response.data
-  if(success){
-    return data
-  }else{
-    Message.error(message)
-    return Promise.reject(new Error(message))
-  }
-},error =>{
+    const { success, message, data } = response.data
+    if (success) {
+      return data
+    } else {
+      Message.error(message)
+      return Promise.reject(new Error(message))
+    }
+  }, error => {
     Message.error(error.message)   // 提示错误信息
     return Promise.reject(error)   // 返回执行错误，让当前的执行链跳出成功，直接进入catch
-    
- }
+
+  }
 )
+//  导出
 export default service
