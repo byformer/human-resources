050d63f3efc2bc283724307115c0c6452a670068
former <1633852615@qq.com>
2023-01-04 14:29:49 +0800

    完成其他页面路由模块搭建

---
+++ src/router/modules/permission.js
@@ -0,0 +1,16 @@
+// 员工路由规则
+import Layout from "@/layout"
+export default {
+    // 路由规则
+    path:'/permission', // 路由地址
+    name:'permission', // 这里会在后面做权限时用到
+    component:Layout,
+    children:[{
+        path:'',  // 这里不用写，什么不写表示 /employees ，不但有layout => 员工主页
+        component:()=>import('@/views/permission'),
+        // 路由元信息，其实就是储存数据的对象，可以放任何内容
+        meta:{
+            title:"权限管理", // 这里用title是因为左侧导航读取了这里的title属性
+        }
+    }]
+}
