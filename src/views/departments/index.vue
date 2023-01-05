71ccb4a8a99935542e58d4c4b5b4167f98e3ba9f
former <1633852615@qq.com>
2023-01-05 11:22:24 +0800

    实现静态组件结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,20 +1,102 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 组织架构内容 - 头部 -->
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px"
+        >
+          <el-col>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span> 操作<i class="el-icon-arrow-down" /> </span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+            <!-- 右侧内容 -->
+          </el-col>
+        </el-row>
+        <!-- 放置一个el-tree -->
+        <el-tree
+          :data="departs"
+          :props="defaultProps"
+          :default-expand-all="true"
+        >
+        <!-- 传入一个插槽，有多少循环多少 -->
+             <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px;width:100%"
+          slot-scope="{data}"
+        >
+          <el-col>
+            <span>{{data.name}}</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>{{data.manger}}</el-col>
+              <el-col>
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span> 操作<i class="el-icon-arrow-down" /> </span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                     <el-dropdown-item>编辑子部门</el-dropdown-item>
+                      <el-dropdown-item>删除子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+            <!-- 右侧内容 -->
+          </el-col>
+        </el-row>
+         </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      departs: [
+        { name: "总裁办",manger:'董事长', children: [{ name: "董事会",manger:'副董事长' }] },
+        { name: "行政部",manger:'行政专用'},
+        { name: "人事部" ,manger:'人事专用'},
+      ],
+      defaultProps: {
+        label: "name", // 表示从这个属性现实内容
+       
+      },
+      
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
 
