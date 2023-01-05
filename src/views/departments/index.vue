09ef4146bf7c96b614ec8a1216184713467834bd
former <1633852615@qq.com>
2023-01-05 14:51:02 +0800

    将数据转换树形结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,91 +4,55 @@
       <!-- 组织架构内容 - 头部 -->
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
-        <el-row
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px"
-        >
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <!-- 两个内容 -->
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /> </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-            <!-- 右侧内容 -->
-          </el-col>
-        </el-row>
+        <TreeTools :treeNode="company" :isRoot="true" />
         <!-- 放置一个el-tree -->
         <el-tree
           :data="departs"
           :props="defaultProps"
           :default-expand-all="true"
         >
-        <!-- 传入一个插槽，有多少循环多少 -->
-             <el-row
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px;width:100%"
-          slot-scope="{data}"
-        >
-          <el-col>
-            <span>{{data.name}}</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <!-- 两个内容 -->
-              <el-col>{{data.manger}}</el-col>
-              <el-col>
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /> </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                     <el-dropdown-item>编辑子部门</el-dropdown-item>
-                      <el-dropdown-item>删除子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-            <!-- 右侧内容 -->
-          </el-col>
-        </el-row>
-         </el-tree>
+          <TreeTools slot-scope="{ data }" :treeNode="data" />
+        </el-tree>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from "./components/tree-tools";
+import { getDepartments } from "@/api/departments";
+import { tranListToTreeData } from "@/utils/index";
 export default {
   data() {
     return {
+      company: {}, // 就是头部的数据结构
       departs: [
-        { name: "总裁办",manger:'董事长', children: [{ name: "董事会",manger:'副董事长' }] },
-        { name: "行政部",manger:'行政专用'},
-        { name: "人事部" ,manger:'人事专用'},
+        {
+          name: "总裁办",
+          manager: "小明",
+          children: [{ name: "董事会", manager: "张三" }],
+        },
+        { name: "行政部", manager: "李四" },
+        { name: "人事部", manager: "小美" },
       ],
       defaultProps: {
         label: "name", // 表示从这个属性现实内容
-       
       },
-      
     };
+  },
+  created() {
+    this.getDepartments(); // 调用自身的方法
+  },
+  methods: {
+    async getDepartments() {
+      const result = await getDepartments();
+      
+      this.company = { name: result.companyName, manager: "负责人" };
+      this.departs = tranListToTreeData(result.depts,'') // 需要将其转换树形
+    },
+  },
+  components: {
+    TreeTools,
   },
 };
 </script>
