6ed0c9a86650e4dd693f9d60894e45e834960b0b
former <1633852615@qq.com>
2023-01-07 11:56:30 +0800

    员工列表数据与分页加载

--- src/api/employees.js
+++ src/api/employees.js
@@ -7,3 +7,15 @@ export function getSimple(){
         url:'/sys/user/simple',
     })
 }
+
+/**
+ * 获取员工的综合列表数据
+ * ***/
+ export function getEmployeeList(params) {
+    return request({
+      url: '/sys/user',
+      params
+    })
+  }
+  
+  
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -5,50 +5,83 @@
         <!-- 左侧显示总记录 -->
         <!-- <span slot="before">共16条记录</span> -->
         <template v-slot:before>
-          <span slot="before">共16条记录</span>
+          <span slot="before">共{{ page.total }}条记录</span>
         </template>
         <!-- 右侧显示按钮 excel导入 excel 导出 新增员工-->
-           <template v-slot:after>
+        <template v-slot:after>
           <el-button size="small" type="success">excel导入</el-button>
           <el-button size="small" type="danger">excel导出</el-button>
           <el-button size="small" type="primary">新增员工</el-button>
         </template>
       </PageTools>
       <!-- 表格组件 -->
-      <el-table>
-            <el-table-column label="序号" sortable="" />
-          <el-table-column label="姓名" sortable="" />
-          <el-table-column label="工号" sortable="" />
-          <el-table-column label="聘用形式" sortable="" />
-          <el-table-column label="部门" sortable="" />
-          <el-table-column label="入职时间" sortable="" />
-          <el-table-column label="账户状态" sortable="" />
-          <el-table-column label="操作" sortable="" fixed="right" width="280">
-            <template>
-              <el-button type="text" size="small">查看</el-button>
-              <el-button type="text" size="small">转正</el-button>
-              <el-button type="text" size="small">调岗</el-button>
-              <el-button type="text" size="small">离职</el-button>
-              <el-button type="text" size="small">角色</el-button>
-              <el-button type="text" size="small">删除</el-button>
-            </template>
-          </el-table-column>
+      <el-table v-loading="loading" :data="list">
+        <el-table-column type="index" label="序号" sortable="" />
+        <el-table-column label="姓名" prop="username" sortable="" />
+        <el-table-column label="工号" prop="workNumber" sortable="" />
+        <el-table-column label="聘用形式" prop="formOfEmployment" sortable="" />
+        <el-table-column label="部门" prop="departmentName" sortable="" />
+        <el-table-column label="入职时间" prop="timeOfEntry" sortable="" />
+        <el-table-column label="账户状态" prop="enableState" sortable="" />
+        <el-table-column label="操作" sortable="" fixed="right" width="280">
+          <template>
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">转正</el-button>
+            <el-button type="text" size="small">调岗</el-button>
+            <el-button type="text" size="small">离职</el-button>
+            <el-button type="text" size="small">角色</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
-     <el-row type="flex" justify="center" align="middle" style="height:60px">
-       <el-pagination layout="prev,pager,next" />
-     </el-row>
+      <el-row type="flex" justify="center" align="middle" style="height: 60px">
+        <el-pagination
+          layout="prev,pager,next"
+          :current-page="page.page"
+          :page-size="page.size"
+          :total="page.total"
+          @current-change="changePage"
+        />
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
+import { getEmployeeList } from '@/api/employees'
 export default {
-
+  data() {
+    return {
+      list: [], // 接收数组
+      page: {
+        page: 1,
+        size: 10,
+        total: 0 // 分页总数
+      },
+      loading: false // 控制遮罩层
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      this.loading = true
+      const { total, rows } = await getEmployeeList(this.page)
+      this.page.total = total
+      this.list = rows
+      this.loading = false
+    },
+    // 最新的页面
+    changePage(newPage){
+      this.page.page = newPage // 赋值最新的页面
+      this.getEmployeeList() // 重新拉取数据
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
 
