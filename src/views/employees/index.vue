563fa829359f6476a889571216463fba82e93679
former <1633852615@qq.com>
2023-01-09 15:44:11 +0800

    导出excel基本结构

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,7 @@
         <!-- 右侧显示按钮 excel导入 excel 导出 新增员工-->
         <template v-slot:after>
           <el-button size="small" type="success" @click="$router.push('/import')">excel导入</el-button>
-          <el-button size="small" type="danger">excel导出</el-button>
+          <el-button size="small" type="danger" @click="exportData">excel导出</el-button>
           <el-button size="small" type="primary" @click="showDialog = true ">新增员工</el-button>
         </template>
       </PageTools>
@@ -121,7 +121,39 @@ export default {
       } catch (err) {
         console.log(err);
       }
-    }
+    },exportData(){
+        const headers = {
+        '姓名': 'username',
+        '手机号': 'mobile',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+      // 导出excel
+      import('@/vendor/Export2Excel').then(async excel =>{
+        // excel是文件的导出对象
+        // 导出
+        // 获取员工的接口
+      const {rows} = await getEmployeeList({page:1,size:this.page.total})
+        const data= this.formatJson(headers,rows)
+          excel.export_json_to_excel({
+            header:Object.keys(headers),
+            data
+          })
+      })
+    },
+  formatJson(headers,rows){
+ /*   return rows.map(item =>{
+      // item 是一个对象
+     return Object.keys(headers).map(key =>{
+       return item[headers[key]]
+      })
+
+    }) */
+    return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
+  }
   }
 }
 </script>
